91ea2b5e2a0aa453f3282934ab898ca6ecb414f4
maaksT <maksymilian.tokarz@salesmanago.com>
2023-02-27 21:56:01 +0100

    Initial commit

---
+++ src/main/java/pl/makst/todoapp/model/SqlTaskRepository.java
@@ -0,0 +1,10 @@
+package pl.makst.todoapp.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+interface SqlTaskRepository extends TaskRepository,JpaRepository<Task,Integer> {
+
+}
---
+++ src/main/java/pl/makst/todoapp/model/TaskRepository.java
@@ -0,0 +1,18 @@
+package pl.makst.todoapp.model;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository {
+    List<Task> findAll();
+    Page<Task> findAll(Pageable pageable);
+    Optional<Task> findById(Integer i);
+    Task save(Task entity);
+    boolean existsById(Integer id);
+    List<Task> findByDone(@Param("state") boolean done);
+
+}
